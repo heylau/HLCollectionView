8a631ca451fcfd44939cb28a4d8d5ca920c72a0e
heylau <iheylau@icloud.com>
2017-04-07 04:57:44 +0800

    pod

---
+++ Classes/UIView+HLFrame.h
@@ -0,0 +1,18 @@
+//
+//  UIView+HLFrame.h
+//  Got Job
+//
+//  Created by heylau on 17/2/8.
+//  Copyright © 2017年 hey lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (HLFrame)
+@property (nonatomic, assign) CGFloat hl_x;
+@property (nonatomic, assign) CGFloat hl_y;
+@property (nonatomic, assign) CGFloat hl_width;
+@property (nonatomic, assign) CGFloat hl_height;
+@property (nonatomic, assign) CGFloat hl_centerX;
+@property (nonatomic, assign) CGFloat hl_centetY;
+@end
